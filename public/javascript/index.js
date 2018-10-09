5f64a6fdb5e445bc6bf14263f296cb1ffb14a833
Derek Sims <DerekSims@wr-130-64-218-81.medford.tufts.edu>
2018-10-08 21:54:19 -0400

    implemented the front end data structure

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,3 +1,5 @@
+chats = [];
+
 function initialize() {
     // Get all elements with class="chatcontent" and hide them
     chatcontent = document.getElementsByClassName("chatcontent");
@@ -10,6 +12,30 @@ function initialize() {
     for (i = 0; i < username.length; i++) {
         username[i].className = username[i].className.replace(" active", "");
     }
+
+    exampleChatUsage();
+}
+
+
+function exampleChatUsage() {
+
+    // Creates new chat with a user called 'chat1'
+    newChat('chat1');
+    console.log(chats);
+
+    // Example of creating a new chat with a duplicate user name
+    // will result in an error
+    newChat('chat1');
+
+    // Adds a message saying 'hello' from the 'chat1' user to the chat 
+    messageObject = { role: 'user', message: 'hello', timestamp: new Date()}
+    addMessage('chat1', messageObject);
+    console.log(chats);
+
+    // Example of adding a message to a chat that does not exists
+    // will also result in an error
+    addMessage('chat2', messageObject);
+
 }
 
 function toggleChat(evt, chatName) {
@@ -40,4 +66,33 @@ function toggleChat(evt, chatName) {
     }
 }
 
-window.onload = initialize();
+
+// Given a user identifier, creates a new chat for that user if the identifier is unique
+// and logs an error if it is a duplicate
+function newChat(user) {
+    validUser = true;
+    for (chat of chats) {
+        if (user == chat.user) {
+            console.log(Error('Cannot have multiple chats with identical user identifiers'));
+            validUser = false;
+        }
+    }
+    if (validUser) {
+        chats.push({ user: user, messageStream: { messages: [], active: true } });
+    }
+}
+
+// Given a user identifier and a messageObject, appends the message object to that user's
+// chat if it exists, logs an error if that user chat doesn't exist 
+function addMessage(user, messageObject) {
+    foundUser = false;
+    for (chat of chats) {
+        if (user == chat.user) {
+            chat.messageStream.messages.push(messageObject);
+            foundUser = true;
+        }
+    }
+    if (!foundUser) {
+        console.log(Error('User with given identifier could not be found'));
+    }
+}
