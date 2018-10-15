c38fea16e422f93be79cda7217cbd2693a42212d
Sinclair Meggitt <scmeggitt@gmail.com>
2018-10-14 20:25:10 -0400

    Updated server.js to use passport

--- server.js
+++ server.js
@@ -1,13 +1,45 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
+const passport = require('passport');
+const mongoose = require('mongoose');
+const LocalStrategy = require('passport-local').Strategy;
+const flash = require('connect-flash');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+var db = mongoose.connection;
+mongoose.connect('mongodb://localhost/admin');
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(session({ secret: 'secret' }));
+app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// passport config
+var Admin = require('./models/AdminModel');
+passport.use(new LocalStrategy(Admin.authenticate()));
+passport.serializeUser(Admin.serializeUser());
+passport.deserializeUser(Admin.deserializeUser());
+
+
+function ensureAuthenticated(req, res, next) {
+	//return next();
+    if (req.isAuthenticated()) { return next(); }
+    res.redirect('/public/login');
+}
 
 app.get('/', function(req, res) {
 	res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
 
-app.get('/admin', function(req, res) {
+app.get('/admin', ensureAuthenticated, function(req, res) {
 	res.sendFile('admin.html', {root: path.join(__dirname, 'public')});
 });
 
