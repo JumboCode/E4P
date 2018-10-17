72450aea8058b5b11522353253cf5b8196b0fe75
anikaaga <anika.agarwal@me.com>
2018-10-17 12:45:22 -0400

    added to server

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ app.get('/admin', function(req, res) {
 	res.sendFile('admin.html', {root: path.join(__dirname, 'public')});
 });
 
+app.get('/login', function(req, res) {
+	res.sendFile('login_page.html', {root: path.join(__dirname, 'public')});
+});
+
+app.get('/help', function(req, res) {
+	res.sendFile('help_page.html', {root: path.join(__dirname, 'public')});
+});
+
+app.get('/:folder/:file', function(req, res) {
+	res.sendFile(req.params.file, {root: path.join(__dirname, 'public', req.params.folder)});
+});
+
 app.listen(process.env.PORT || 3000, function() {
   	console.log('Node app is running on port 3000');
 });
