0e4df44e86a360f8ed44b6b842f5f3cab8301553
Erena Inoue <ErenaCInoue@Erenas-MacBook-Air.local>
2018-12-05 12:30:06 -0500

    time stamp edited

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -2,7 +2,7 @@
  * Return a message div based on the role and message string.
  */
 function createMessageDiv(side, message) {
-    return "<div class= 'container'><div class='" + side + "-chat-bubble'> " + message + "</div></div>";
+    return "<div class= 'container'><div class='" + side + "-chat-bubble'> " + message + "</div><div class= 'time'> "+ "11:45am" +" </div></div>";
 }
 
 /*
