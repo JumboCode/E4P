82a4f9103686b452a9a830605a2f9754904683c0
Alex Johnson <alex.johnson969@gmail.com>
2019-01-28 17:26:00 -0500

    Remove outdated logs

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -94,15 +94,11 @@ function updateChat(messageObj) {
   chatbox = document.getElementById("chatbox");
   if (messageObj.role == 'admin') {
       messageSide = 'left';
-      //newMessage = "<div class='chat_admin'><div class='received_msg'><p>"+messageObj.message+"</p></div></div>"
   }
   else {
-      console.log(messageObj.message);
       messageSide = 'right';
-      //newMessage = "<div class='chat_user'><div class='sent_msg'><p>"+messageObj.message+"</p></div></div>"
   }
   newMessage = createMessageDiv(messageSide, messageObj.message);
-  console.log(messages.innerHTML);
   messages.innerHTML = messages.innerHTML + newMessage;
   messages.scrollTop = messages.scrollHeight - messages.clientHeight;
 }
