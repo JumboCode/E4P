3735d33e91b91dd7088d339146f243ebd56536a0
Erena Inoue <ErenaCInoue@Erenas-MacBook-Air.local>
2019-02-05 15:39:59 -0500

    styled the timestamp to float it left and right.

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -2,7 +2,8 @@
  * Return a message div based on the role and message string.
  */
 function createMessageDiv(side, message) {
-    return "<div class= 'container'><div class='" + side + "-chat-bubble'> " + message + "</div><div class= 'time'> "+ "11:45am" +" </div></div>";
+	console.log(side)
+    return "<div class= 'container'><div class='" + side + "-chat-bubble'> " + message + " </div><div class= '" + side + "-time'> " + getTimeString(new Date()) + " </div></div>";
 }
 
 /*
