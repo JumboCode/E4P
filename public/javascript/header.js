25df14896abb0c30520279e04f3bb79950a1ec06
Alex Johnson <alex.johnson969@gmail.com>
2019-02-12 15:48:24 -0500

    set tab size to 2 from 4 on header.js

--- public/javascript/header.js
+++ public/javascript/header.js
@@ -1,34 +1,34 @@
 function generateAdminHeader() {
-	generateHeader('admin');
+  generateHeader('admin');
 }
 
 function generateUserHeader() {
-	generateHeader('user');
+  generateHeader('user');
 }
 
 function generateLoginHeader() {
-	generateHeader('login');
+  generateHeader('login');
 }
 
 function generateHeader(role) {
-	home = '/';
-	login = '/admin/login';
-	logout = '/admin/logout';
+  home = '/';
+  login = '/admin/login';
+  logout = '/admin/logout';
 
-	if (role === 'user') {
-		buttons ="<a href='/help' target='_blank'>Help</a>" +
-		"<a href='http://sites.tufts.edu/ears4peers/contact-us/' target='_blank'>Feedback</a>" +
-		"<a href=" + login + ">Login</a>";
-	} else if (role === 'login') {
-		buttons = "<a href='/help' target='_blank'>Help</a>" +
-		"<a href='http://sites.tufts.edu/ears4peers/contact-us/' target='_blank'>Feedback</a>";
-	} else {
-	  buttons = "<a href=" + logout + ">Logout</a>";
-	}
+  if (role === 'user') {
+    buttons ="<a href='/help' target='_blank'>Help</a>" +
+    "<a href='http://sites.tufts.edu/ears4peers/contact-us/' target='_blank'>Feedback</a>" +
+    "<a href=" + login + ">Login</a>";
+  } else if (role === 'login') {
+    buttons = "<a href='/help' target='_blank'>Help</a>" +
+    "<a href='http://sites.tufts.edu/ears4peers/contact-us/' target='_blank'>Feedback</a>";
+  } else {
+    buttons = "<a href=" + logout + ">Logout</a>";
+  }
 
-	document.getElementsByClassName("topnav")[0].innerHTML = 
-		"<a class='active' href='" + home + "'>E4P</a>"
-		+ "<div class='topnav-right'>"
-		+ 	buttons
-		+ "</div>";
+  document.getElementsByClassName("topnav")[0].innerHTML = 
+    "<a class='active' href='" + home + "'>E4P</a>"
+    + "<div class='topnav-right'>"
+    +   buttons
+    + "</div>";
 }
