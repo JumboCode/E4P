d59243c75a773a42a8840db7a4dbd00a518eb329
Steven <steven.song98@gmail.com>
2019-04-15 21:07:58 -0400

    move from sqlite to postgres

--- auth/auth.js
+++ auth/auth.js
@@ -1,14 +1,18 @@
 const crypto = require('crypto');
 const bcrypt = require('bcryptjs');
-const sqlite3 = require('sqlite3');
 const nodemailer = require('nodemailer');
 const querystring = require('querystring');
 const assert = require('assert');
 
-const DB_NAME = 'db.sqlite3';
 const SCHEMA_VERSION = 1;
 
-let db = new sqlite3.Database(DB_NAME);
+const { Client } = require('pg');
+const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+db.connect();
 
 // TODO add register routes
 // TODO add testing
@@ -17,17 +21,17 @@ let db = new sqlite3.Database(DB_NAME);
 //        DB Schema Check
 ///////////////////////////////////////////////////////////////////////
 
-db.get('SELECT version FROM schema_version', (err, row) => {
+db.query('SELECT version FROM schema_version', (err, res) => {
   if (err) {
     // create table
-    db.run('CREATE TABLE schema_version (version integer)', (err) => {
+    db.query('CREATE TABLE schema_version (version integer)', (err) => {
       if (err) throw err;
-      db.run('INSERT INTO schema_version VALUES (?)', SCHEMA_VERSION, (err) => { if (err) throw err; });
+      db.query('INSERT INTO schema_version VALUES ($1)', [SCHEMA_VERSION], (err) => { if (err) throw err; });
     });
-    db.run('CREATE TABLE users (username text primary key, password text, email text)', (err) => { if (err) throw err; });
-    db.run('CREATE TABLE change_requests (request text unique, expires integer, username text unique, foreign key(username) references users (username))', (err) => { if (err) throw err; });
-    db.run('CREATE TABLE login_attempts (ip text unique, attempts integer, next_attempt integer)', (err) => { if (err) throw err; });
-  } else if (!row || row.version != SCHEMA_VERSION) {
+    db.query('CREATE TABLE users (username text primary key, password text, email text)', (err) => { if (err) throw err; });
+    db.query('CREATE TABLE change_requests (request text unique, expires integer, username text unique, foreign key(username) references users (username))', (err) => { if (err) throw err; });
+    db.query('CREATE TABLE login_attempts (ip text unique, attempts integer, next_attempt integer)', (err) => { if (err) throw err; });
+  } else if (!res || res.rows[0].version != SCHEMA_VERSION) {
     // error bad schema version
     throw new Error('Database Schema Version Mismatch');
   }
@@ -41,11 +45,11 @@ const SCRUBBER_INTERVAL = process.env.scrubInt || 5; // hours
 
 let db_scrubber = setInterval(() => {
   let timestamp = Date.now();
-  db.run('DELETE FROM login_attempts WHERE next_attempt < ?', timestamp, (err) => {
+  db.query('DELETE FROM login_attempts WHERE next_attempt < $1', [timestamp], (err) => {
     if (err) throw err;
   });
 
-  db.run('DELETE FROM change_requests WHERE expires < ?', timestamp, (err) => {
+  db.query('DELETE FROM change_requests WHERE expires < $1', [timestamp], (err) => {
     if (err) throw err;
   });
 }, SCRUBBER_INTERVAL * 3600000);
@@ -90,18 +94,18 @@ function start_password_change(email) {
   let expires = date.setMinutes(date.getMinutes() + VALID_DURATION);
   let request = crypto.randomBytes(16).toString('hex');
 
-  db.get('SELECT username FROM users WHERE email = ?', email, (err, row) => {
+  db.query('SELECT username FROM users WHERE email = $1', [email], (err, res) => {
     if (err) throw err;
 
     // TODO maybe alert user attempt was bad?
-    if (!row) {
+    if (!res) {
       return;
     }
 
-    db.run('REPLACE INTO change_requests VALUES (?, ?, ?)', request, expires, row.username, (err) => {
+    db.query('REPLACE INTO change_requests VALUES ($1, $2, $3)', [request, expires, res.rows[0].username], (err) => {
       if (err) throw err;
 
-      sendMail(row.username, email, request);
+      sendMail(res.rows[0].username, email, request);
     });
   });
 }
@@ -115,15 +119,15 @@ function valid_password_change(request, cb) {
   assert(typeof request === 'string');
   let timestamp = Date.now();
 
-  db.get('SELECT username, expires FROM change_requests WHERE request = ?', request, (err, row) => {
+  db.query('SELECT username, expires FROM change_requests WHERE request = $1', [request], (err, res) => {
     if (err) throw err;
 
-    if (row) {
-      if (timestamp < row.expires) {
-        return cb(row.username);
+    if (res) {
+      if (timestamp < res.rows[0].expires) {
+        return cb(res.rows[0].username);
       }
 
-      db.run('DELETE FROM change_requests WHERE request = ?', request, (err) => {
+      db.query('DELETE FROM change_requests WHERE request = $1', [request], (err) => {
         if (err) throw err;
       });
     }
@@ -139,11 +143,11 @@ function change_password(username, password) {
   // autogenerates salt with default of 10 rounds
   let hash = bcrypt.hashSync(password);
 
-  db.run('UPDATE users SET password = ? WHERE username = ?', hash, username, (err) => {
+  db.query('UPDATE users SET password = $1 WHERE username = $2', [hash, username], (err) => {
     if (err) throw err;
   });
 
-  db.run('DELETE FROM change_requests WHERE username = ?', username, (err) => {
+  db.query('DELETE FROM change_requests WHERE username = $1', [username], (err) => {
     if (err) throw err;
   });
 }
@@ -164,7 +168,7 @@ function register_user(username, email, password) {
   let hash = bcrypt.hashSync(password);
 
   // TODO switch out replace and do actual checks for conflicts
-  db.run('REPLACE INTO users (username, email, password) VALUES (?, ?, ?)', username, email, hash, (err) => {
+  db.query('REPLACE INTO users (username, email, password) VALUES ($1, $2, $3)', [username, email, hash], (err) => {
     if (err) throw err;
   });
 }
@@ -182,7 +186,7 @@ const RETRY_WAIT_DURATION = process.env.retryWait || 5; // minutes
 function delete_login_attempt(ip) {
   assert(typeof ip === 'string');
 
-  db.run('DELETE FROM login_attempts WHERE ip = ?', ip, (err) => {
+  db.query('DELETE FROM login_attempts WHERE ip = $1', [ip], (err) => {
     if (err) throw err;
   });
 }
@@ -195,7 +199,7 @@ function increment_attempt(ip, prev_attempts, cb) {
   let attempts = prev_attempts + 1;
   let next_attempt = date.setMinutes(date.getMinutes() + RETRY_WAIT_DURATION);
 
-  db.run('REPLACE INTO login_attempts (ip, attempts, next_attempt) VALUES (?, ?, ?)', ip, attempts, next_attempt, (err) => {
+  db.query('REPLACE INTO login_attempts (ip, attempts, next_attempt) VALUES ($1, $2, $3)', [ip, attempts, next_attempt], (err) => {
     if (err) throw err;
 
     return cb();
@@ -206,7 +210,7 @@ function can_attempt_login(ip, cb) {
   assert(typeof ip === 'string');
   let timestamp = Date.now();
 
-  db.serialize(() => {
+  // db.serialize(() => {
     // insert or replace handles if this is the first attempt and no entry exists
     //
     // ip value is always just the passed in ip
@@ -220,7 +224,7 @@ function can_attempt_login(ip, cb) {
     //              login before and if we've already tried more than allowed, 
     //              we leave the timestamp unchanged
 
-    db.run('REPLACE INTO login_attempts (ip, attempts, next_attempt) \
+    db.query('REPLACE INTO login_attempts (ip, attempts, next_attempt) \
             VALUES (?,\
                     CASE WHEN EXISTS (SELECT 1 FROM login_attempts WHERE ip = ?) AND \
                               (SELECT next_attempt FROM login_attempts WHERE ip = ?) > ? \
@@ -234,25 +238,25 @@ function can_attempt_login(ip, cb) {
                     END)', 
            ip, ip, ip, timestamp, ip, ip, ip, MAX_ATTEMPTS, ip, timestamp + RETRY_WAIT_DURATION * 60 * 1000, (err) => {
       if (err) throw err;
-    });
 
-    db.get('SELECT attempts, next_attempt FROM login_attempts WHERE ip = ?', ip, (err, row) => {
+    db.query('SELECT attempts, next_attempt FROM login_attempts WHERE ip = $1', [ip], (err, res) => {
       if (err) throw err;
 
-      if (!row) throw new Error('This isn\'t possible');
+      if (!res) throw new Error('This isn\'t possible');
 
       // we only need to check attempts because the query above resets attempts when enough time has elapsed
-      if (row.attempts <= MAX_ATTEMPTS) {
-        // console.log(ip + ' allowed attempt, this is attempt ' + (row.attempts + 1));
+      if (res.rows[0].attempts <= MAX_ATTEMPTS) {
+        // console.log(ip + ' allowed attempt, this is attempt ' + (res.rows[0] + 1));
         return cb(true);
       }
       
       // can't log in yet, wait time milliseconds
       // console.log(ip + ' denied attempt');
-      let wait_time = row.next_attempt - timestamp;
+      let wait_time = res.rows[0].next_attempt - timestamp;
       return cb(false, wait_time);
     });
-  });
+    });
+  // });
 }
 
 module.exports.can_attempt_login = can_attempt_login;
@@ -266,17 +270,17 @@ function strategy(username, password, done) {
   assert(typeof username === 'string');
   assert(typeof password === 'string');
 
-  db.get('SELECT username, password FROM users WHERE username = ?', username, (err, row) => {
+  db.query('SELECT username, password FROM users WHERE username = $1', [username], (err, res) => {
     if (err) throw err;
 
-    if (!row) return done(null, false);
+    if (!res) return done(null, false);
 
-    bcrypt.compare(password, row.password, (err, match) => {
+    bcrypt.compare(password, res.rows[0].password, (err, match) => {
       if (err) throw err;
 
       if (!match) return done(null, false);
 
-      return done(null, row);
+      return done(null, { username: res.rows[0].username, password: res.rows[0].password });
     });
   });
 }
@@ -288,9 +292,9 @@ function serialize(user, done) {
 function deserialize(username, done) {
   assert(typeof username === 'string');
 
-  db.get('SELECT username FROM users WHERE username = ?', username, (err, row) => {
-    if (!row) return done(null, false);
-    return done(null, row);
+  db.query('SELECT username FROM users WHERE username = $1', [username], (err, res) => {
+    if (!res) return done(null, false);
+    return done(null, { username: res.rows[0].username });
   });
 }
 
