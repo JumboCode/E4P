bcd39095e48389448db2f6ca2188d8f4adc564e1
Luke Taylor <luke@deentaylor.com>
2021-11-08 19:18:16 -0500

    Display better default hours

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -12,7 +12,7 @@ function connectWithStoredID() {
   }
 }
 
-let operatingHours = "7pm - 2am";
+let operatingHours = "7pm–7am";
 $.getJSON('/hours')
   .done((data) => {
     operatingHours = data.hours;
